e1eb53041fd94e2a3385479588097a05fc080906
Senaektr <senaektiricioglu@gmail.com>
2025-06-07 10:57:36 +0300

    weights are changed

--- main.c
+++ main.c
@@ -33,9 +33,9 @@
 #define NUMBER_OF_GENERATIONS 20000
 
 // Katsayılar sonrasında öneme göre değişecek
-#define W1 0.5
-#define W2 0.5
-#define W3 0.5
+#define W1 0.2
+#define W2 0.4
+#define W3 0.4
 #define PENALTY 10
 #define CROSSOVER_TYPE 3 // 0: order, 1: problem specific, 2: similarity crossover minimize conflict, 3: similarity crossover fitness related
 #define MUTATION_TYPE 4  // 0: type1, 1: type2, 2: type3, 3: type4, 4: type5
